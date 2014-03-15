1fdd70c9d59587ba2b3a6c9c66b0bc24550b9d3f
David Furlong <dvfurlong@googlemail.com>
2014-03-15 18:07:10 +0000

    Added bountyList + more

---
+++ js/UI.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	
+	$('#add-bounty').click(function(){
+		document.location.href = "addBounty.html";
+	});
+
+	$("#bounty-list").tablesorter(); 
+
+});
+
+
+
+// filters bounties on search
+function filterBounties(){
+
+}
