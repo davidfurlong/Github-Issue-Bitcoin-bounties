fcba5d8dca4f185ef740a2e2d58ecdacaf22446b
Greg O'Connor <gregory@oconnor.name>
2014-03-15 19:02:04 +0000

    Implement issues.html

--- js/Issue.js
+++ js/Issue.js
@@ -1,10 +1,11 @@
 define([
 
 ], function() {
-    function Issue(id, name, uri, bounty) {
+    function Issue(id, name, uri, language, bounty) {
         this.id = id;
         this.name = name;
         this.uri = uri;
+        this.language = language;
         this.bounty = bounty;
     }
     return Issue;
--- js/main-issues.js
+++ js/main-issues.js
@@ -4,10 +4,25 @@ define(["common",
     require([
         "jquery",
         "bootstrap", 
+        "tablesorter",
         "server/ServerAPI"
-    ], function($, bootstrap, ServerAPI) {
-        var API = new ServerAPI();
-        console.log(API.getAllIssues());
+    ], function($, bootstrap, tablesorter, ServerAPI) {
+        console.log("Loaded");
+
+        var serverAPI = new ServerAPI();
+        var issueList = serverAPI.getAllIssues();
+
+        for (var i = 0; i < issueList.length; i++) {
+            var issue = issueList[i];
+            var html = "<tr>";
+            html += "<td>" + issue.name + "</td>";
+            html += "<td>" + issue.uri + "</td>";
+            html += "<td>" + "$" + issue.bounty * 100 + "</td>";
+            html += "<td>" + issue.bounty + "</td>";
+            html += "</tr>";
+            $('#issue-list > tbody:last').after(html);
+            console.log(issue);
+        }
     });
 
 });
