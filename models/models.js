8cff71727307d4902fa538de040470d2549cbd26
James Thompson <jmt158@gmail.com>
2014-03-15 21:56:22 +0000

    initial bitcoin stuff

--- models/models.js
+++ models/models.js
@@ -25,7 +25,25 @@ Bounty = sequelize.define('Bounty', {
 
 Issue.hasMany(Bounty, {as: "Bounties"});
 
+Address = sequelize.define('Address', {
+  address: Sequelize.STRING,
+  privkey: Sequelize.STRING,
+  confirmedAmount: Sequelize.INTEGER,
+  unconfirmedAmount: Sequelize.INTEGER
+})
+
+Transactions = sequelize.define('Transactions', {
+   amount: Sequelize.INTEGER,
+   confirmed: Sequelize.BOOLEAN,
+   txid: Sequelize.STRING
+})
+
+Transactions.hasOne(Address, {as: "Address"})
+Address.hasOne(Bounty, {as: "Bounty"})
+
 sequelize.sync()
 
+exports.Address = Address
+exports.Transactions = Transactions
 exports.Issue = Issue
 exports.Bounty = Bounty
---
+++ run.bat
@@ -0,0 +1,2 @@
+SET DATABASE_URL=postgres://postgres:root@localhost:5432/spur
+node bin/www
