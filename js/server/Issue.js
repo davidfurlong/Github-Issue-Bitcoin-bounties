4c462f0a6adcff4800d6e9ac54162b4802f49fc8
Greg O'Connor <gregory@oconnor.name>
2014-03-15 17:30:17 +0000

    Actually remote the .ts files

--- js/server/Issue.js
+++
@@ -1,6 +0,0 @@
-var ServerAPI = (function () {
-    function ServerAPI() {
-        this.SERVER_URI = "https://git-spur.herokuapp.com/";
-    }
-    return ServerAPI;
-})();
--- ts/Issue.ts
+++
@@ -1,5 +0,0 @@
-class Issue {
-    constructor(public id: number, public name: string, public uri: string, public bounty: number) {
-    
-    }
-}
